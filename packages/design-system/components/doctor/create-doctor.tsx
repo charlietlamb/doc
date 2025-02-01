d3fa23b15c1e09690617f1ec931d47568624ac2d
Charlie Lamb <charlielamb20@icloud.com>
2025-02-01 17:19:13 +0000

    add theme css

---
+++ packages/design-system/components/doctor/create-doctor.tsx
@@ -0,0 +1,3 @@
+export default function CreateDoctor() {
+  return <div>create-doctor</div>
+}
--- packages/design-system/components/header/header.tsx
+++ packages/design-system/components/header/header.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 export default function Header() {
   return (
-    <div className="p-4 flex justify-between items-center">
+    <div className="p-4 flex justify-between items-center border-b">
       <Link href="/">
         <h2 className="text-lg font-bold">doc</h2>
       </Link>
