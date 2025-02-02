2f11636519dc3ac27644153b3e05a3d7942225ea
Charlie Lamb <charlielamb20@icloud.com>
2025-02-02 10:15:44 +0000

    page reset build test

--- apps/web/app/(routes)/slots/page.tsx
+++ apps/web/app/(routes)/slots/page.tsx
@@ -2,14 +2,13 @@ import React from 'react'
 import Slots from '@doc/design-system/components/slots/slots'
 import { getDoctors } from '@doc/design-system/actions/doctors/get-doctors'
 
-export const dynamic = 'force-dynamic'
-
 export default async function SlotsPage() {
-  const doctors = await getDoctors()
-  const doctorsWithDates = doctors.map((doctor) => ({
-    ...doctor,
-    createdAt: new Date(doctor.createdAt),
-    updatedAt: new Date(doctor.updatedAt),
-  }))
-  return <Slots doctors={doctorsWithDates} />
+  return null
+  // const doctors = await getDoctors()
+  // const doctorsWithDates = doctors.map((doctor) => ({
+  //   ...doctor,
+  //   createdAt: new Date(doctor.createdAt),
+  //   updatedAt: new Date(doctor.updatedAt),
+  // }))
+  // return <Slots doctors={doctorsWithDates} />
 }
---
+++ packages/design-system/atoms/doctor/doctor-atoms.ts
@@ -0,0 +1,5 @@
+import { atom } from 'jotai'
+import { Doctor } from '@doc/database/schema/doctors'
+
+export const doctorAtom = atom<Doctor | null>(null)
+export const dateAtom = atom<Date | null>(null)
