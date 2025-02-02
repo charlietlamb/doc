78ddeb8693b48c7e256abf166c550b3214e3745b
Charlie Lamb <charlielamb20@icloud.com>
2025-02-02 11:23:46 +0000

    adding force dynamic to routes

--- apps/web/app/(routes)/slots/page.tsx
+++ apps/web/app/(routes)/slots/page.tsx
@@ -1,5 +1,6 @@
 import Slots from '@doc/design-system/components/slots/slots'
 
+export const dynamic = 'force-dynamic'
 export default async function SlotsPage() {
   return <Slots />
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,7 @@
 import Index from '@doc/design-system/components/index'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
   return <Index />
 }
