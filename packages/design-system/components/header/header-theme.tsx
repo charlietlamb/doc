78289703b74037b4fb85c5bdaefb107e8a8ad15c
Charlie Lamb <charlielamb20@icloud.com>
2025-02-03 21:48:22 +0000

    fix theme issue

--- packages/design-system/components/header/header-theme.tsx
+++ packages/design-system/components/header/header-theme.tsx
@@ -13,7 +13,7 @@ export function HeaderTheme() {
   const { resolvedTheme } = useTheme()
   return (
     <Popover>
-      <PopoverTrigger asChild>
+      <PopoverTrigger>
         <div className="h-full aspect-square flex items-center justify-center hover:bg-muted transition-colors duration-300 cursor-pointer text-muted-foreground hover:text-foreground size-12">
           {resolvedTheme === 'light' ? <Sun /> : <Moon />}
         </div>
